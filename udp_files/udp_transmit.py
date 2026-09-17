1004d56cb0aa017c4cbb7ef7aa525c143be3c387
DiegoDeLaVara <dd048@uark.edu>
2026-09-17 17:53:47 -0500

    Set up the udp sockets and, gave option to connect up different networks

--- udp_files/udp_transmit.py
+++ udp_files/udp_transmit.py
@@ -2,19 +2,33 @@ import socket
 import time
 
 SOURCE_IP = "127.0.0.1"     # Your local interface ( I will use 192.168.1.2)
-PORT = 7500
+PORT = 7501
 BROADCAST_IP = "127.0.0.255"  # for testing with the hardware, I will use 192.168.1.255
 MESSAGE = "Hello UDP broadcast"
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def broadcast_equipment_code(equipment_code, broadcast_ip):
 
-# Allow broadcasting
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    # Allow broadcasting
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-print(f"Broadcasting from {SOURCE_IP} to {BROADCAST_IP}:{PORT}")
+    message = str(equipment_code)
 
-while True:
-    sock.sendto(MESSAGE.encode(), (BROADCAST_IP, PORT))
-    print("Sent:", MESSAGE)
-    time.sleep(1)  # send every 1 second
+    # Encodes string to bytes with utf-8
+    sock.sendto(message.encode("utf-8"), (broadcast_ip, PORT))
+
+    print("Broadcasted equipment code:", message)
+
+    sock.close()
+
+def select_network(code):
+    
+
+    choice = input("Enter network: ")
+    broadcast_equipment_code(str(code), choice)
+    
+
+#test
+select_network("ABCD123")
+
